c7701ab8cd53f6e304c8dd132cabe454c752eb78
Betina Dalope <betina.devwork@gmail.com>
2020-03-28 20:11:09 -0400

    Updates

---
+++ precache-manifest.c2e3b0998afc95cc1c6b9c960a1e7a80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c321ff9a28b8e2bf16c9d91147f44c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ce95bf4f98853bbfdace",
+    "url": "/portfolio/static/css/2.69507bff.chunk.css"
+  },
+  {
+    "revision": "91ee4566b5c0fe66b761",
+    "url": "/portfolio/static/css/main.c16b16d9.chunk.css"
+  },
+  {
+    "revision": "ce95bf4f98853bbfdace",
+    "url": "/portfolio/static/js/2.6e6a5539.chunk.js"
+  },
+  {
+    "revision": "f19e55d60ef6e34dd3cd36ff2cdbc08f",
+    "url": "/portfolio/static/js/2.6e6a5539.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91ee4566b5c0fe66b761",
+    "url": "/portfolio/static/js/main.3882a949.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
