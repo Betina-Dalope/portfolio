8a4137522e0476c3317011ffefff331e85f0f1db
Betina Dalope <betina.devwork@gmail.com>
2020-04-01 18:21:45 -0400

    Updates

---
+++ precache-manifest.9cde35aa48663154b373bb4aded1ad89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125865d6546fd94db9aceda688f1c80a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8cb7ff30be192543897d",
+    "url": "/portfolio/static/css/2.4515a016.chunk.css"
+  },
+  {
+    "revision": "172ad581ddccff7cae0b",
+    "url": "/portfolio/static/css/main.ea341861.chunk.css"
+  },
+  {
+    "revision": "8cb7ff30be192543897d",
+    "url": "/portfolio/static/js/2.d246eb08.chunk.js"
+  },
+  {
+    "revision": "ede086bb53f22f963aa73678716c7697",
+    "url": "/portfolio/static/js/2.d246eb08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "172ad581ddccff7cae0b",
+    "url": "/portfolio/static/js/main.c023fd99.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.657f78c89d414cc1a4ad45e906174f4a.js"
+  "/portfolio/precache-manifest.9cde35aa48663154b373bb4aded1ad89.js"
 );
 
 self.addEventListener('message', (event) => {
